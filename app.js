0e85db4645ac9a5623ad69973a53fb95a83a8292
Evan Moore <etmoore@gmail.com>
2015-12-12 16:28:32 -0700

    add 4 quotes

---
+++ app.js
@@ -0,0 +1,18 @@
+var quotes = [
+  {
+    body: "Rudeness is the weak man's imitation of strength.",
+    author: "Eric Hoffer"
+  },
+  {
+    body: "If you're not making mistakes, then you're not doing anything.",
+    author: "John Wooden"
+  },
+  {
+    body: "To lament that we shall not be alive a hundred years hence, is the same folly as to be sorry we were not alive a hundred years ago.",
+    author: "Michel de Montaigne"
+  },
+  {
+    body: "Teaching is like trying to hold 35 corks underwater at once.",
+    author: "Mark Twain"
+  },
+];
